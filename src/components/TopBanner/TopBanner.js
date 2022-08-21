394c848928536ae35dbb6b842f7acf899f16acc2
saifuldeveloper <mdsaiful77336@gmail.com>
2022-08-21 18:33:09 +0600

    uncomple layout

---
+++ src/components/TopBanner/TopBanner.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from 'react'
+import '../../asset/css/custom.css'
+import '../../asset/css/bootstrap.min.css'
+import { Container } from "react-bootstrap";
+
+
+export default class TopBanner extends Component {
+  render() {
+    return (
+        <Fragment>
+           <Container className="TopFixedBanner">
+
+           </Container>
+
+        </Fragment>    
+    
+    )
+  }
+}
