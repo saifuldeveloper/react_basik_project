99ff40baa48aa015eedd8a45d1e9ed6bdf8d21e1
saifuldeveloper <mdsaiful77336@gmail.com>
2022-08-23 11:46:03 +0600

    summary section complete

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,6 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { Component } from "react";
 import { Fragment } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row,Card} from "react-bootstrap";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+
+
 
 export default class Summary extends Component {
   render() {
@@ -11,19 +15,32 @@ export default class Summary extends Component {
             <Container className="SummarySection text-center">
               <Row>
                 <Col lg={8} md={6} sm={12}>
-                  <Row>
+                  <Row className="CountSection ">
                     <Col>
-                         <h1 className="CountNumber">1000</h1>
-                         <h4>Total Project</h4>
+                      <h1 className="CountNumber">1000</h1>
+                      <h4 className="CountTitle">Total Project</h4>
+                      <hr className="bg-white  w-25" />
                     </Col>
                     <Col>
-                         <h1>1000</h1>
-                         <h4>Total Project</h4>
+                      <h1 className="CountNumber">1000</h1>
+                      <h4 className="CountTitle">Total Project</h4>
+                      <hr className="bg-whitt w-25" />
                     </Col>
-                       
                   </Row>
                 </Col>
-                <Col lg={4} md={6} sm={12}></Col>
+                <Col lg={4} md={6} sm={12}>
+                  <Card className="CardWork">
+                    <Card.Body>
+                      <Card.Title className="CardTitle ">HOW I WORK</Card.Title>
+                      <Card.Text>
+                        <p className="CardSubTitle "><FontAwesomeIcon className="IconBullet" icon={faCheckCircle} />Requrement Gathaing</p>
+                        <p className="CardSubTitle "><FontAwesomeIcon  className="IconBullet" icon={faCheckCircle} />System Analysis</p>
+                        <p className="CardSubTitle "><FontAwesomeIcon className="IconBullet" icon={faCheckCircle} />Coding Testing</p>
+                        <p className="CardSubTitle "><FontAwesomeIcon className="IconBullet" icon={faCheckCircle} />Implementatioon</p>
+                      </Card.Text>                    
+                    </Card.Body>
+                  </Card>
+                </Col>
               </Row>
             </Container>
           </div>
