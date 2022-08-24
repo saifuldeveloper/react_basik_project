ce416bab8868621ec6c143827b3e26ddd1021fec
saifuldeveloper <mdsaiful77336@gmail.com>
2022-08-24 11:15:20 +0600

    Video Section Complete

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,21 +1,19 @@
 import React, { Component, Fragment } from "react";
-import { Container, Row, Col ,Button,Modal} from "react-bootstrap";
+import { Container, Row, Col, Button, Modal } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+import "video-react/dist/video-react.css";
+import { BigPlayButton, Player } from "video-react";
 
 export default class Video extends Component {
-
-     constructor() {
-        super();
-        this.state = {
-            show: false,
-        };
-     }
-     MdalClose =()=>this.setState({show: false});
-     ModalOpen =()=>this.setState({show: true});
-
-
-
+  constructor() {
+    super();
+    this.state={
+      show: false
+    }
+  }
+  MdalClose=()=>this.setState({ show: false })
+  ModalOpen=()=>this.setState({ show: true })
 
   render() {
     return (
@@ -35,20 +33,26 @@ export default class Video extends Component {
                   essentially unchanged. It was popularised in
                 </p>
                 <p>
-                  <FontAwesomeIcon onClick={this.ModalOpen} className="PlayBtn"icon={faPlayCircle}></FontAwesomeIcon>
+                  <FontAwesomeIcon
+                    onClick={this.ModalOpen}
+                    className="PlayBtn"
+                    icon={faPlayCircle}
+                  ></FontAwesomeIcon>
                 </p>
               </div>
             </Col>
           </Row>
         </Container>
         {/* ====================MOdal Section ============= */}
-        <Modal show={this.state.show} onClick={this.MdalClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal size="lg" show={this.state.show} onHide={this.MdalClose}>
+          <Modal.Body>
+            <Player>
+              <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+              <BigPlayButton position="center"></BigPlayButton>
+            </Player>
+          </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={this.MdalClose}>
+            <Button variant="primary" onClick={this.MdalClose}>
               Close
             </Button>
           </Modal.Footer>
